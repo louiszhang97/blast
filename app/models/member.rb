55b6990034a3fa473cc700c4d159a396033313b2
Louis Zhang <louiszhang97@gmail.com>
2017-07-11 18:45:44 -0700

    fixed bugs

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,5 +2,5 @@ class Member < ApplicationRecord
 	validates :name, presence: true
 	validates :phone, presence: true 
 	validates :name, length: {in: 1..20}
-	validates :phone, length: {is: 10}
+	validates :phone, length: {is: 12}
 end
